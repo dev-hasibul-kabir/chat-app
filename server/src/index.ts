c36dfe1d8329a69c137b5802860aae059f5c032e
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-08-19 22:57:49 +0600

    monngo db database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 5000");
+  connectDB();
 });

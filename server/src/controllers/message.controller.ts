ab7ddff32c5d05bc7cea461e3d882ffe313c536a
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-08-23 20:07:16 +0600

    get all other users except me

--- server/src/controllers/message.controller.ts
+++ server/src/controllers/message.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
 
 interface MessageController {
   getUsers: (req: Request, res: Response) => void;
@@ -6,7 +7,17 @@ interface MessageController {
 
 const messageController: MessageController = {
   getUsers: async (req, res) => {
-    res.send("Get users");
+    try {
+      const myId = req.user._id;
+
+      const users = await User.find({ _id: { $ne: myId } }).select(
+        "-password -createdAt -updatedAt"
+      );
+      res.status(200).json(users);
+    } catch (error) {
+      console.log("Error fetching users:", error);
+      res.status(500).json({ message: "Server error" });
+    }
   },
 };
 

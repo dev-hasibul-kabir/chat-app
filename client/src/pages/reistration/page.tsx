a6cb61a7d7c78b70b07caae44e03c73e2bacebe0
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-08-28 20:15:57 +0600

    Signup form handling

--- client/src/pages/reistration/page.tsx
+++ client/src/pages/reistration/page.tsx
@@ -1,6 +1,34 @@
 import { Link } from "react-router";
+import { z } from "zod";
+import Button from "../../components/Button";
+import { Controller, useForm } from "react-hook-form";
+import Input from "../../components/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Registration() {
+  const signinSchema = z.object({
+    name: z.string().nonempty("Name is required!"),
+    email: z.string().min(1, "Email is required!").email(),
+    password: z
+      .string()
+      .nonempty("Password is required!")
+      .min(6, "Password must be at least 6 characters"),
+  });
+
+  type FormData = z.infer<typeof signinSchema>;
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
   return (
     <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg p-8 w-full max-w-md">
       <h1 className="text-center text-white text-2xl font-bold mb-6">
@@ -9,36 +37,56 @@ export default function Registration() {
 
       <h2 className="text-xl text-white font-semibold mb-6">Signup</h2>
 
-      <div className="mb-4">
-        <label className="block text-sm text-white mb-2">Name</label>
-        <input
-          type="name"
-          placeholder="John Doe"
-          className="w-full px-4 py-2 rounded-lg bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      <form
+        className="space-y-6"
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <Input
+              label="Name"
+              type="text"
+              placeholder="John Doe"
+              value={field.value}
+              onChange={field.onChange}
+              error={errors.name?.message}
+            />
+          )}
         />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-sm text-white mb-2">Email</label>
-        <input
-          type="email"
-          placeholder="username@gmail.com"
-          className="w-full px-4 py-2 rounded-lg bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <Input
+              label="Email"
+              type="email"
+              placeholder="user@gmail.com"
+              value={field.value}
+              onChange={field.onChange}
+              error={errors.email?.message}
+            />
+          )}
         />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-sm text-white mb-2">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full px-4 py-2 rounded-lg bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <Input
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+              value={field.value}
+              onChange={field.onChange}
+              error={errors.password?.message}
+            />
+          )}
         />
-      </div>
-
-      <button className="w-full mt-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white font-semibold">
-        Signup
-      </button>
+        <Button type="submit" className="w-full">
+          Sign up
+        </Button>
+      </form>
       <p className="mt-4 text-center text-sm text-white/80">
         Already registered?{" "}
         <Link to="/login" className="underline">

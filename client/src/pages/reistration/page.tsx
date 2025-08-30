2472094500ba293ecb6dd212769017622a873e1c
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-08-30 19:59:28 +0600

    client registration

--- client/src/pages/reistration/page.tsx
+++ client/src/pages/reistration/page.tsx
@@ -1,34 +1,52 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { z } from "zod";
 import Button from "../../components/Button";
 import { Controller, useForm } from "react-hook-form";
 import Input from "../../components/Input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useAuthStore } from "../../store/useAuthStore";
+
+const signupSchema = z.object({
+  name: z.string().nonempty("Name is required!"),
+  email: z.string().min(1, "Email is required!").email(),
+  password: z
+    .string()
+    .nonempty("Password is required!")
+    .min(6, "Password must be at least 6 characters"),
+});
+type FormData = z.infer<typeof signupSchema>;
 
 export default function Registration() {
-  const signinSchema = z.object({
-    name: z.string().nonempty("Name is required!"),
-    email: z.string().min(1, "Email is required!").email(),
-    password: z
-      .string()
-      .nonempty("Password is required!")
-      .min(6, "Password must be at least 6 characters"),
-  });
-
-  type FormData = z.infer<typeof signinSchema>;
+  const navigate = useNavigate();
+  const { register, loading } = useAuthStore();
 
   const {
     handleSubmit,
     control,
     formState: { errors },
   } = useForm<FormData>({
-    resolver: zodResolver(signinSchema),
+    resolver: zodResolver(signupSchema),
     defaultValues: {
       name: "",
       email: "",
       password: "",
     },
   });
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const { success, message } = await register(data);
+      if (success) {
+        console.log(message);
+        navigate("/", { replace: true });
+      } else {
+        console.log(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg p-8 w-full max-w-md">
       <h1 className="text-center text-white text-2xl font-bold mb-6">
@@ -37,10 +55,7 @@ export default function Registration() {
 
       <h2 className="text-xl text-white font-semibold mb-6">Signup</h2>
 
-      <form
-        className="space-y-6"
-        onSubmit={handleSubmit((data) => console.log(data))}
-      >
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
         <Controller
           name="name"
           control={control}
@@ -83,7 +98,7 @@ export default function Registration() {
             />
           )}
         />
-        <Button type="submit" className="w-full">
+        <Button type="submit" className="w-full" loading={loading}>
           Sign up
         </Button>
       </form>

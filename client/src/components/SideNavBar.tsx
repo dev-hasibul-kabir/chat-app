c03cee6ec0261cf651626db2d4b22e1218d3db9d
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-09-13 22:25:36 +0600

    handle logout

--- client/src/components/SideNavBar.tsx
+++ client/src/components/SideNavBar.tsx
@@ -1,15 +1,33 @@
-import { NavLink } from "react-router";
-import { FaComments, FaUserAlt, FaCog } from "react-icons/fa";
+import { NavLink, useNavigate } from "react-router";
+import { LuUser } from "react-icons/lu";
+import { HiOutlineChatBubbleLeftRight } from "react-icons/hi2";
+import { MdLogout } from "react-icons/md";
 import { useAuthStore } from "../store/useAuthStore";
 
 export default function SideNavBar() {
-  const { user } = useAuthStore();
+  const { user, logout } = useAuthStore();
+  const navigate = useNavigate();
 
   const navItems = [
-    { id: "chats", label: "Chats", icon: <FaComments />, path: "/" },
-    { id: "profile", label: "Profile", icon: <FaUserAlt />, path: "/profile" },
-    { id: "settings", label: "Settings", icon: <FaCog />, path: "/settings" },
+    {
+      id: "chats",
+      label: "Chats",
+      icon: <HiOutlineChatBubbleLeftRight />,
+      path: "/message",
+    },
+    { id: "profile", label: "Profile", icon: <LuUser />, path: "/profile" },
+    // { id: "settings", label: "Settings", icon: <FaCog />, path: "/settings" },
+    { id: "logout", label: "Logout", icon: <MdLogout />, path: "" },
   ];
+
+  function handleLogout() {
+    try {
+      logout();
+      navigate("/login", { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="h-screen w-24 flex flex-col items-center py-4 bg-white/10 backdrop-blur-md border-r border-white/20">
@@ -29,20 +47,30 @@ export default function SideNavBar() {
 
       {/* Nav Items */}
       <div className="flex flex-col gap-6">
-        {navItems.map((item) => (
-          <NavLink
-            key={item.id}
-            to={item.path}
-            className={({ isActive }) =>
-              `flex flex-col items-center text-sm ${
-                isActive ? "text-cyan-400" : "text-white"
-              }`
-            }
-          >
-            <span className="text-2xl mb-1">{item.icon}</span>
-            <span>{item.label}</span>
-          </NavLink>
-        ))}
+        {navItems.map((item) =>
+          item.id == "logout" ? (
+            <button
+              className="flex flex-col items-center text-sm text-white cursor-pointer"
+              onClick={handleLogout}
+            >
+              <span className="text-2xl mb-1">{item.icon}</span>
+              <span>{item.label}</span>
+            </button>
+          ) : (
+            <NavLink
+              key={item.id}
+              to={item.path}
+              className={({ isActive }) =>
+                `flex flex-col items-center text-sm ${
+                  isActive ? "text-cyan-400" : "text-white"
+                }`
+              }
+            >
+              <span className="text-2xl mb-1">{item.icon}</span>
+              <span>{item.label}</span>
+            </NavLink>
+          )
+        )}
       </div>
     </div>
   );

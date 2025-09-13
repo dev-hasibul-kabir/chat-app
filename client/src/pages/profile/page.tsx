75bdec3d2586e58ca5d7ef1da0460ef436d85455
dev-hasibul-kabir <hasibul.kabir.dev@gmail.com>
2025-09-13 21:33:58 +0600

    responsive profile page

--- client/src/pages/profile/page.tsx
+++ client/src/pages/profile/page.tsx
@@ -29,9 +29,9 @@ export default function Profile() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen text-white">
-      <div className="shadow-lg rounded-2xl p-8 w-2/4 text-center backdrop-blur-sm bg-white/10">
-        <div className="relative w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 mx-auto border-4 border-blue-300 rounded-full">
+    <div className="flex items-center justify-center min-h-screen text-white p-4">
+      <div className="shadow-lg rounded-2xl p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl text-center backdrop-blur-sm bg-white/10">
+        <div className="relative w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 mx-auto border-4 border-blue-300 rounded-full">
           {loading ? (
             <img
               className="w-full h-full rounded-full object-cover blur-xs"
@@ -58,12 +58,14 @@ export default function Profile() {
           />
           <IoCamera
             onClick={() => inputRef.current?.click()}
-            className="absolute bottom-3 right-3 size-4 md:size-8 text-blue-600 bg-white/20 backdrop-blur-md p-1 rounded-full cursor-pointer hover:border hover:border-blue-500"
+            className="absolute bottom-2 right-2 sm:bottom-3 sm:right-3 size-5 sm:size-6 md:size-8 text-blue-600 bg-white/20 backdrop-blur-md p-1 rounded-full cursor-pointer hover:border hover:border-blue-500"
           />
         </div>
 
-        <h2 className="mt-4 text-2xl font-semibold">{user?.name}</h2>
-        <p className="mt-2">{user?.email}</p>
+        <h2 className="mt-4 text-lg sm:text-xl md:text-2xl font-semibold">
+          {user?.name}
+        </h2>
+        <p className="mt-1 sm:mt-2 text-sm sm:text-base">{user?.email}</p>
       </div>
     </div>
   );
